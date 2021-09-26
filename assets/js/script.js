30ca8ecb5b8255b828f78022c24aaea16d76f093
Chris Zuniga <zunigachristian81@gmail.com>
2021-09-25 22:20:47 -0500

    getting save buttons to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,25 @@
 // moment to view current date
 var currentDate = moment();
-$("#currentDay").text(currentDate.format("dddd, MMMM Do"));
+$("#currentDay").text(currentDate.format("dddd, MMM Do"));
+
+
+function nine() {
+   // var taskContent = document.querySelector("#9am");
+    //var savedContent = document.querySelector("9am");
+    var saveBtn9 = document.querySelector("#save9");
+
+    saveBtn9.addEventListener("click", function(){
+        changeSavedContent();
+        console.log("test");
+    });
+
+   // savedContent.textContent = localStorage.getItem("data");
+    //taskContent.value = localStorage.getItem("data");
+        
+    function changeSavedContent() {
+        //localStorage.setItem("data", taskContent.value);
+        //savedContent.textContent = taskContent.value;
+        console.log("test");
+    };    
+};    
+        
